f811d02c36b2dbe11488e4e57ac20585221b07bb
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-03 08:28:45 -0700

    create font manager

---
+++ lib/core/resources/font_manager.dart
@@ -0,0 +1,418 @@
+import 'package:flutter/material.dart';
+class FontConstants {
+  static const String fontFamily = "Style";
+  static const String gastromondFamily = "Gastromond";
+  static const String elzaFamily = "Elza";
+}
+class FontWeightManager {
+  static const FontWeight light100 = FontWeight.w100;
+  static const FontWeight light200 = FontWeight.w200;
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight bold800 = FontWeight.w800;
+  static const FontWeight bold900 = FontWeight.w900;
+}
+
+class FontSize {
+  static const double s0 = 0;
+  static const double s1 = 1.0;
+  static const double s2 = 2.0;
+  static const double s3 = 3.0;
+  static const double s4 = 4.0;
+  static const double s5 = 5.0;
+  static const double s6 = 6.0;
+  static const double s7 = 7.0;
+  static const double s8 = 8.0;
+  static const double s9 = 9.0;
+
+  static const double s10 = 10.0;
+  static const double s11 = 11.0;
+  static const double s12 = 12.0;
+  static const double s13 = 13.0;
+  static const double s14 = 14.0;
+  static const double s15 = 15.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s19 = 19.0;
+
+  static const double s20 = 20.0;
+  static const double s21 = 21.0;
+  static const double s22 = 22.0;
+  static const double s23 = 23.0;
+  static const double s24 = 24.0;
+  static const double s25 = 25.0;
+  static const double s26 = 26.0;
+  static const double s27 = 27.0;
+  static const double s28 = 28.0;
+  static const double s29 = 29.0;
+
+  static const double s30 = 30.0;
+  static const double s31 = 31.0;
+  static const double s32 = 32.0;
+  static const double s33 = 33.0;
+  static const double s34 = 34.0;
+  static const double s35 = 35.0;
+  static const double s36 = 36.0;
+  static const double s37 = 37.0;
+  static const double s38 = 38.0;
+  static const double s39 = 39.0;
+
+  static const double s40 = 40.0;
+}
+
+// i cannot make it like this because i will lose the main feature that i make those classes "Track every call"
+
+// class _AllPaddingManager extends _PaddingManager {}
+//
+// class VerticalPaddingManager extends _AllPaddingManager {}
+//
+// class HorizontalPaddingManager extends _AllPaddingManager {}
+//
+// class StartPaddingManager extends _PaddingManager {}
+//
+// class EndPaddingManager extends _AllPaddingManager {}
+//
+// class TopPaddingManager extends _PaddingManager {}
+//
+// class BottomPaddingManager extends _AllPaddingManager {}
+
+class AllPaddingManager {
+  static const double p0 = 0;
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+
+  static const double p20 = 20.0;
+  static const double p21 = 21.0;
+  static const double p22 = 22.0;
+  static const double p23 = 23.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p26 = 26.0;
+  static const double p27 = 27.0;
+  static const double p28 = 28.0;
+  static const double p29 = 29.0;
+
+  static const double p30 = 30.0;
+  static const double p31 = 31.0;
+  static const double p32 = 32.0;
+  static const double p33 = 33.0;
+  static const double p34 = 34.0;
+  static const double p35 = 35.0;
+  static const double p36 = 36.0;
+  static const double p37 = 37.0;
+  static const double p38 = 38.0;
+  static const double p39 = 39.0;
+
+  static const double p40 = 40.0;
+}
+
+class VerticalPaddingManager {
+  static const double p0 = 0;
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+
+  static const double p20 = 20.0;
+  static const double p21 = 21.0;
+  static const double p22 = 22.0;
+  static const double p23 = 23.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p26 = 26.0;
+  static const double p27 = 27.0;
+  static const double p28 = 28.0;
+  static const double p29 = 29.0;
+
+  static const double p30 = 30.0;
+  static const double p31 = 31.0;
+  static const double p32 = 32.0;
+  static const double p33 = 33.0;
+  static const double p34 = 34.0;
+  static const double p35 = 35.0;
+  static const double p36 = 36.0;
+  static const double p37 = 37.0;
+  static const double p38 = 38.0;
+  static const double p39 = 39.0;
+
+  static const double p40 = 40.0;
+}
+
+class HorizontalPaddingManager {
+  static const double p0 = 0;
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+
+  static const double p20 = 20.0;
+  static const double p21 = 21.0;
+  static const double p22 = 22.0;
+  static const double p23 = 23.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p26 = 26.0;
+  static const double p27 = 27.0;
+  static const double p28 = 28.0;
+  static const double p29 = 29.0;
+
+  static const double p30 = 30.0;
+  static const double p31 = 31.0;
+  static const double p32 = 32.0;
+  static const double p33 = 33.0;
+  static const double p34 = 34.0;
+  static const double p35 = 35.0;
+  static const double p36 = 36.0;
+  static const double p37 = 37.0;
+  static const double p38 = 38.0;
+  static const double p39 = 39.0;
+
+  static const double p40 = 40.0;
+}
+
+class StartPaddingManager {
+  static const double p0 = 0;
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+
+  static const double p20 = 20.0;
+  static const double p21 = 21.0;
+  static const double p22 = 22.0;
+  static const double p23 = 23.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p26 = 26.0;
+  static const double p27 = 27.0;
+  static const double p28 = 28.0;
+  static const double p29 = 29.0;
+
+  static const double p30 = 30.0;
+  static const double p31 = 31.0;
+  static const double p32 = 32.0;
+  static const double p33 = 33.0;
+  static const double p34 = 34.0;
+  static const double p35 = 35.0;
+  static const double p36 = 36.0;
+  static const double p37 = 37.0;
+  static const double p38 = 38.0;
+  static const double p39 = 39.0;
+
+  static const double p40 = 40.0;
+}
+
+class EndPaddingManager {
+  static const double p0 = 0;
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+
+  static const double p20 = 20.0;
+  static const double p21 = 21.0;
+  static const double p22 = 22.0;
+  static const double p23 = 23.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p26 = 26.0;
+  static const double p27 = 27.0;
+  static const double p28 = 28.0;
+  static const double p29 = 29.0;
+
+  static const double p30 = 30.0;
+  static const double p31 = 31.0;
+  static const double p32 = 32.0;
+  static const double p33 = 33.0;
+  static const double p34 = 34.0;
+  static const double p35 = 35.0;
+  static const double p36 = 36.0;
+  static const double p37 = 37.0;
+  static const double p38 = 38.0;
+  static const double p39 = 39.0;
+
+  static const double p40 = 40.0;
+}
+
+class TopPaddingManager {
+  static const double p0 = 0;
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+
+  static const double p20 = 20.0;
+  static const double p21 = 21.0;
+  static const double p22 = 22.0;
+  static const double p23 = 23.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p26 = 26.0;
+  static const double p27 = 27.0;
+  static const double p28 = 28.0;
+  static const double p29 = 29.0;
+
+  static const double p30 = 30.0;
+  static const double p31 = 31.0;
+  static const double p32 = 32.0;
+  static const double p33 = 33.0;
+  static const double p34 = 34.0;
+  static const double p35 = 35.0;
+  static const double p36 = 36.0;
+  static const double p37 = 37.0;
+  static const double p38 = 38.0;
+  static const double p39 = 39.0;
+
+  static const double p40 = 40.0;
+}
+
+class BottomPaddingManager {
+  static const double p0 = 0;
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p3 = 3.0;
+  static const double p4 = 4.0;
+  static const double p5 = 5.0;
+  static const double p6 = 6.0;
+  static const double p7 = 7.0;
+  static const double p8 = 8.0;
+  static const double p9 = 9.0;
+
+  static const double p10 = 10.0;
+  static const double p11 = 11.0;
+  static const double p12 = 12.0;
+  static const double p13 = 13.0;
+  static const double p14 = 14.0;
+  static const double p15 = 15.0;
+  static const double p16 = 16.0;
+  static const double p17 = 17.0;
+  static const double p18 = 18.0;
+  static const double p19 = 19.0;
+
+  static const double p20 = 20.0;
+  static const double p21 = 21.0;
+  static const double p22 = 22.0;
+  static const double p23 = 23.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p26 = 26.0;
+  static const double p27 = 27.0;
+  static const double p28 = 28.0;
+  static const double p29 = 29.0;
+
+  static const double p30 = 30.0;
+  static const double p31 = 31.0;
+  static const double p32 = 32.0;
+  static const double p33 = 33.0;
+  static const double p34 = 34.0;
+  static const double p35 = 35.0;
+  static const double p36 = 36.0;
+  static const double p37 = 37.0;
+  static const double p38 = 38.0;
+  static const double p39 = 39.0;
+
+  static const double p40 = 40.0;
+}
+
