dad9bbec483d55c16cb939783bb9deb958b59122
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-03 08:45:44 -0700

    change import in test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:market_viewer/main.dart';
+import 'package:market_viewer/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
