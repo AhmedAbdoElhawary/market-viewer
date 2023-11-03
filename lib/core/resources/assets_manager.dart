228e8374ccf585154a8344b195115c41533cc3eb
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-03 08:26:18 -0700

    create IconsAssets

---
+++ lib/core/resources/assets_manager.dart
@@ -0,0 +1,8 @@
+const String iconPath = "assets/icons";
+const String imagePath = "assets/images";
+
+class IconsAssets {
+  // static const String noNetwork = "$iconPath/no_network.svg";
+  // static const String noNetwork = "$imagePath/no_network.svg";
+
+}
