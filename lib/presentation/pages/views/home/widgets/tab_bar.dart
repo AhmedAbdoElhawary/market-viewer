4dfaf6a45a45bd0985053ddde3991e266b500aa8
Ahmed Abdo <88978546+AhmedAbdoElhawary@users.noreply.github.com>
2023-11-03 18:32:38 +0200

    create _TabBar

---
+++ lib/presentation/pages/views/home/widgets/tab_bar.dart
@@ -0,0 +1,31 @@
+part of '../home_page.dart';
+
+class _TabBar extends StatelessWidget {
+  const _TabBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        decoration: const BoxDecoration(
+            border: Border(bottom: BorderSide(color: ColorManager.whiteD2))),
+        child: TabBar(
+          isScrollable: true,
+          labelPadding: REdgeInsets.symmetric(horizontal: 10),
+          indicatorSize: TabBarIndicatorSize.label,
+          unselectedLabelStyle:
+              getRegularStyle(fontSize: FontSize.s14, color: ColorManager.grey),
+          labelStyle: getRegularStyle(fontSize: FontSize.s14),
+          tabs: const [
+            Tab(text: "Stock"),
+            Tab(text: "Currencies"),
+            Tab(text: "Cryptocurrencies"),
+            // gold, sliver erc..
+            Tab(text: "Commodities"),
+          ],
+        ),
+      ),
+    );
+  }
+}
