475e3528de785c4a92f7dc187fac6c8c6b1c5b71
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-05 15:26:08 -0800

    create _StockTabBarView

---
+++ lib/presentation/pages/views/home/widgets/stock_view.dart
@@ -0,0 +1,10 @@
+part of '../home_page.dart';
+
+class _StockTabBarView extends StatelessWidget {
+  const _StockTabBarView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: RegularText("First one"));
+  }
+}
