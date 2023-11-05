9e3cc4e13439eeee8782722006f9a803fe41a4d6
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-05 15:26:17 -0800

    create _CryptoCurrenciesTabBarView

---
+++ lib/presentation/pages/views/home/widgets/cryptocurrency_view.dart
@@ -0,0 +1,10 @@
+part of '../home_page.dart';
+
+class _CryptoCurrenciesTabBarView extends StatelessWidget {
+  const _CryptoCurrenciesTabBarView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: RegularText("Third one"));
+  }
+}
