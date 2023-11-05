7fb8c3744172e7c85c253b191a4c47210eccdd95
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-05 15:26:20 -0800

    create _CommoditiesTabBarView

---
+++ lib/presentation/pages/views/home/widgets/commodities_view.dart
@@ -0,0 +1,10 @@
+part of '../home_page.dart';
+
+class _CommoditiesTabBarView extends StatelessWidget {
+  const _CommoditiesTabBarView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: RegularText("Forth one"));
+  }
+}
