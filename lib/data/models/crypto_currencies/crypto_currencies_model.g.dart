3c5bc8ecf4f1178f53ba43785cdf6f56007e2c5a
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-13 08:57:42 -0800

    add Quote with QuoteUSD of .g

--- lib/data/models/crypto_currencies/crypto_currencies_model.g.dart
+++ lib/data/models/crypto_currencies/crypto_currencies_model.g.dart
@@ -35,4 +35,31 @@ CryptoCurrenciesData _$CryptoCurrenciesDataFromJson(
           (json['selfReportedMarketCap'] as num?)?.toDouble(),
       tvlRatio: (json['tvlRatio'] as num?)?.toDouble(),
       lastUpdated: json['lastUpdated'] as String?,
+      quote: json['quote'] == null
+          ? null
+          : Quote.fromJson(json['quote'] as Map<String, dynamic>),
+    );
+
+Quote _$QuoteFromJson(Map<String, dynamic> json) => Quote(
+      usd: json['USD'] == null
+          ? null
+          : QuoteUSD.fromJson(json['USD'] as Map<String, dynamic>),
+    );
+
+QuoteUSD _$QuoteUSDFromJson(Map<String, dynamic> json) => QuoteUSD(
+      price: (json['price'] as num?)?.toDouble(),
+      volume24h: (json['volume24h'] as num?)?.toDouble(),
+      volumeChange24h: (json['volumeChange24h'] as num?)?.toDouble(),
+      percentChange1h: (json['percentChange1h'] as num?)?.toDouble(),
+      percentChange24h: (json['percentChange24h'] as num?)?.toDouble(),
+      percentChange7d: (json['percentChange7d'] as num?)?.toDouble(),
+      percentChange30d: (json['percentChange30d'] as num?)?.toDouble(),
+      percentChange60d: (json['percentChange60d'] as num?)?.toDouble(),
+      percentChange90d: (json['percentChange90d'] as num?)?.toDouble(),
+      marketCap: (json['marketCap'] as num?)?.toDouble(),
+      marketCapDominance: (json['marketCapDominance'] as num?)?.toDouble(),
+      fullyDilutedMarketCap:
+          (json['fullyDilutedMarketCap'] as num?)?.toDouble(),
+      tvl: (json['tvl'] as num?)?.toDouble(),
+      lastUpdated: json['lastUpdated'] as String?,
     );
