b1309a3371beb7a0ff01ab1b2e3bf5164edd725d
ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-11-13 08:57:32 -0800

    add Quote with QuoteUSD

--- lib/data/models/crypto_currencies/crypto_currencies_model.dart
+++ lib/data/models/crypto_currencies/crypto_currencies_model.dart
@@ -30,6 +30,7 @@ class CryptoCurrenciesData {
   double? selfReportedMarketCap;
   double? tvlRatio;
   String? lastUpdated;
+  Quote? quote;
 
   CryptoCurrenciesData({
     this.id,
@@ -48,8 +49,57 @@ class CryptoCurrenciesData {
     this.selfReportedMarketCap,
     this.tvlRatio,
     this.lastUpdated,
+    this.quote,
   });
 
   factory CryptoCurrenciesData.fromJson(Map<String, dynamic> json) =>
       _$CryptoCurrenciesDataFromJson(json);
+}
+
+@JsonSerializable(includeIfNull: true, createToJson: false)
+class Quote {
+  @JsonKey(name: "USD")
+  QuoteUSD? usd;
+
+  Quote({this.usd});
+
+  factory Quote.fromJson(Map<String, dynamic> json) => _$QuoteFromJson(json);
+}
+
+@JsonSerializable(includeIfNull: true, createToJson: false)
+class QuoteUSD {
+  double? price;
+  double? volume24h;
+  double? volumeChange24h;
+  double? percentChange1h;
+  double? percentChange24h;
+  double? percentChange7d;
+  double? percentChange30d;
+  double? percentChange60d;
+  double? percentChange90d;
+  double? marketCap;
+  double? marketCapDominance;
+  double? fullyDilutedMarketCap;
+  double? tvl;
+  String? lastUpdated;
+
+  QuoteUSD({
+    this.price,
+    this.volume24h,
+    this.volumeChange24h,
+    this.percentChange1h,
+    this.percentChange24h,
+    this.percentChange7d,
+    this.percentChange30d,
+    this.percentChange60d,
+    this.percentChange90d,
+    this.marketCap,
+    this.marketCapDominance,
+    this.fullyDilutedMarketCap,
+    this.tvl,
+    this.lastUpdated,
+  });
+
+  factory QuoteUSD.fromJson(Map<String, dynamic> json) =>
+      _$QuoteUSDFromJson(json);
 }
